e4692d6c9163a8e626c0d459f0118b43fa6007a4
Darla Nova <jolynekujoismywife06@gmail.com>
2025-10-18 17:05:29 +0800

    Added name attribute to input fields

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ const closeBtn = document.querySelector(".close");
 closeBtn.addEventListener("click", ()=>{
     dialog.close();
 })
+
+
+const myLib = [];
+
+
+function Book(){
+
+}
+
+function addBooktoLib(){
+    //create book and store new book object to an array
+    // book objects should have a unique id,
+let book = {
+    title
+}
+    
+}
