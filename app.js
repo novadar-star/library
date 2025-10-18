60174a81d1479cf7c2d90023420489eb3a0471ad
Darla Nova <jolynekujoismywife06@gmail.com>
2025-10-18 16:35:02 +0800

    Added dialog close to cancel button

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const showBtn = document.querySelector("#addnewBook");
 showBtn.addEventListener("click", ()=>{
     dialog.showModal();
 });
+
+const closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", ()=>{
+    dialog.close();
+})
